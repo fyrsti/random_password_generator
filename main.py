a2760ba9a3d341c52b4ddfb75368e5e4eda2788a
fyrsti <wonderdeveloper@gmail.com>
2021-02-13 23:53:28 +0200

    Add function signatures.

---
+++ main.py
@@ -0,0 +1,33 @@
+# import random as ra
+
+
+def randomize_symbols() -> str:
+    pass
+
+
+def construct_password() -> None:
+    pass
+
+
+def ask_wrapper() -> None:
+    pass
+
+
+def asknumber() -> int:
+    pass
+
+
+def askyesno() -> bool:
+    pass
+
+
+def get_input() -> str:
+    pass
+
+
+def main() -> None:
+    pass
+
+
+if __name__ == '__main__':
+    main()
