283d34c834092a36722894df3d6442fe7763891b
fyrsti <wonderdeveloper@gmail.com>
2021-02-14 00:44:26 +0200

    Write implementations for "gather" functions. Construct function is still empty. Create a decorator for printing messages in "gather" functions.

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 # import random as ra
 
 
+def send_function_message(text: str):
+    def outer(f):
+        def wrapper(*args: tuple, **kwargs: dict):
+            print(text)
+            return f(*args, **kwargs)
+        return wrapper
+    return outer
+
+
 def randomize_symbols() -> str:
     pass
 
@@ -9,24 +18,38 @@ def construct_password() -> None:
     pass
 
 
-def ask_wrapper() -> None:
-    pass
+@send_function_message("Укажите длину пароля в символах [от 3 до 32]")
+def asknumber(minborder: float, maxborder: float) -> int:
+    while 1:
+        response = get_input()
+        if validate_number(response, minborder, maxborder): return int(response)
+        else:
+            print(f'Вы указали неверное значение. Число должно быть в промежутке от {minborder} до {maxborder}')
 
 
-def asknumber() -> int:
-    pass
+def validate_number(input_str: str, minborder: float, maxborder: float) -> bool:
+    try:
+        if float(input_str) < minborder or float(input_str) > maxborder or int(input_str) != float(input_str):
+            raise ValueError
+    except ValueError:
+        return False
+    else:
+        return True
 
 
 def askyesno() -> bool:
-    pass
+    return True if get_input() == '+' else False
 
 
 def get_input() -> str:
-    pass
-
+    return input()
 
 def main() -> None:
-    pass
+    password_length: int = asknumber(3, 32)
+    print("\n[+ - для положительного ответа]\n[Нажмите \"Enter\" для отрицательного ответа]\n")
+    include_numbers = send_function_message("Использовать числа в пароле?: ")(askyesno)()
+    include_punc = send_function_message("Использовать знаки пунктуации в пароле?: ")(askyesno)()
+    register = send_function_message("Учитывать регистр символов в пароле?: ")(askyesno)()
 
 
 if __name__ == '__main__':
